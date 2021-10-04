f2f065ecceb37940fe173376e7998c69aa24b86a
Markinger <markus.j.mueller94@icloud.com>
2021-10-04 21:32:26 +0200

    disabled generate tweets post for now

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,8 +17,8 @@
 	// Breakpoints.
 		breakpoints({
 			default:   ['1681px',   null       ],
-			xlarge:    ['1281px',   '1680px'   ],
-			large:     ['981px',    '1280px'   ],
+			xlarge:    ['1300px',   '1680px'   ],
+			large:     ['981px',    '1299px'   ],
 			medium:    ['737px',    '980px'    ],
 			small:     ['481px',    '736px'    ],
 			xsmall:    ['361px',    '480px'    ],
